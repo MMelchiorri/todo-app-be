7d8d41cfebe0e9a462b6bf11f5ce41b883b9a9cd
MMelchiorri <mm.melchiorri@gmail.com>
2025-08-05 15:16:37 +0200

    feat: add unique id field to user schema with UUID generation

--- src/schemas/Users.js
+++ src/schemas/Users.js
@@ -1,7 +1,15 @@
 const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
+const { v4: uuidv4 } = require("uuid");
+
 const UserSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+    default: () => uuidv4(),
+  },
   username: {
     type: String,
     required: true,
