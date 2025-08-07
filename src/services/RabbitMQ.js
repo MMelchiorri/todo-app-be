5e06a514abeaedca40ca3a2e4b3f8757f3eb344e
MMelchiorri <mm.melchiorri@gmail.com>
2025-08-07 14:46:53 +0200

    feat: configure RabbitMQ exchange and queue in the application

--- src/services/RabbitMQ.js
+++ src/services/RabbitMQ.js
@@ -16,6 +16,10 @@ class RabbitMQ {
     const connection = await amqp.connect(process.env.RABBITMQ_URI);
     console.log("Connected to RabbitMQ");
     const channel = await connection.createChannel();
+    await channel.assertExchange("todo", "direct", { durable: true });
+    await channel.assertQueue("todo_queue", { durable: true });
+    await channel.bindQueue("todo_queue", "todo", "todo_job");
+    console.log("Exchange and queue created");
   };
 }
 
